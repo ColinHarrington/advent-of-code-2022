e390eb0d5f522c638cb8a48574796e06be0d5769
Colin Harrington <colin.harrington@gmail.com>
2023-08-14 00:54:29 -0500

    Fixing day24 stub

--- rs/src/day24.rs
+++ rs/src/day24.rs
@@ -1,19 +1,22 @@
 use yaah::*;
 
 #[aoc_generator(day24)]
-fn parse_basin(input: &'static str) -> () {
-}
+fn parse_basin(input: &'static str) -> () {}
 
 #[aoc(day24, part1)]
-fn solve_part1() -> i32 {
- -1
+fn solve_part1() -> u32 {
+    0
 }
 
 struct Basin {
     width: u32,
-    height: u32
+    height: u32,
 }
 
 enum Step {
-    UP, DOWN, LEFT, RIGHT, WAIT
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    WAIT,
 }
